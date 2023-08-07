3a66815c4cb0a8347e8c534f8a3dccc2cde772db
Joan Gizzio M <59625288+joanGmcd@users.noreply.github.com>
2023-08-07 18:05:30 -0400

    Update VRMLLoader.js

--- VRMLLoader.js
+++ VRMLLoader.js
@@ -33,7 +33,7 @@ import {
 	Vector2,
 	Vector3
 } from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.module.js';
-import chevrotain from './chevrotain.module.min.js';
+import chevrotain from 'https://unpkg.com/chevrotain/lib/chevrotain.min.mjs';
 
 
 class VRMLLoader extends Loader {
