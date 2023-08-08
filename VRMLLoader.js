9b7ddc4509e4b4cc09ed99a568b9bc64d2e77c72
Joan Gizzio M <59625288+joanGmcd@users.noreply.github.com>
2023-08-07 20:40:49 -0400

    Update VRMLLoader.js

--- VRMLLoader.js
+++ VRMLLoader.js
@@ -33,7 +33,7 @@ import {
 	Vector2,
 	Vector3
 } from './three.module.js';
-import * as chevrotain from './chevrotain.module.min.js';
+import * as chevrotain from './chevrotain.min.js';
 
 
 class VRMLLoader extends Loader {
